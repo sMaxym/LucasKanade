5f8c39f582fcb2d688364b8dd6ec56d379c1cd03
smaxym <02smaxym@gmail.com>
2020-04-25 20:37:27 +0300

    added rgb to grayscale

--- py/lucaskanade.py
+++ py/lucaskanade.py
@@ -5,9 +5,16 @@ import matplotlib.pyplot as plt
 
 import sys
 
+
+def rgb2gs(img):
+    return np.dot(img[..., :3], [0.2125, 0.7154, 0.0721])
+
+
+
 img = imageio.imread(sys.argv[1])
 
-img = np.mean(img, axis=2)
+# img = np.mean(img, axis=2)
+img = rgb2gs(img)
 img = img.T
 
 imageio.imwrite('test.jpg', img)
