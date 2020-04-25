376fbb4ae3178a762702e4bb0781df08cd61667f
smaxym <02smaxym@gmail.com>
2020-04-25 19:51:55 +0300

    Cpp proj init

---
+++ lib/cmake-build-debug/CMakeFiles/lib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lib.dir/main.cpp.o"
+  "lib"
+  "lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
