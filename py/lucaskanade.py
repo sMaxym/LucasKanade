83bb6ba16ccaa2f9860c452c8bff8674e91f7ef5
smaxym <02smaxym@gmail.com>
2020-05-13 13:13:14 +0300

    added gaussian filter for x y and t axis

--- py/lucaskanade.py
+++ py/lucaskanade.py
@@ -7,11 +7,12 @@ import matplotlib.colors as colors
 import sys
 
 from sklearn.feature_extraction import image
-from math import exp
+from math import exp, ceil
 
 
-PART_X, PART_Y = 20, 25
+PART_X, PART_Y = 20, 20
 PARTITION = (PART_Y, PART_X)
+BLOCK_SHAPE = (20, 20)
 
 def gaussian2d(x, y, x_0=0, y_0=0, sigma_x=1, sigma_y=1, amp=1):
     return amp * exp(-( (x - x_0) ** 2 / (2 * sigma_x ** 2) + (y - y_0) ** 2 / (2 * sigma_y ** 2) ))
@@ -42,6 +43,20 @@ def img_block(img, partirion=(1, 1)):
         if prev_x >= img.shape[1]:
             prev_x %= img.shape[1]
             prev_y += block_h
+            
+def img_block_pixelvice(img, block_shape=(5, 5)):
+    rows, cols = img.shape
+    block_rows, block_cols = block_shape 
+    col_parts = ceil(cols / block_cols)
+    row_parts = ceil(rows / block_rows)
+    for row in range(row_parts):
+        for col in range(col_parts):
+            diff_row, diff_col = block_rows, block_cols
+            if (row + 1) * block_rows >= rows:
+                diff_row = rows - row * block_rows
+            if (col + 1) * block_cols >= cols:
+                diff_col = cols - col * block_cols
+            yield img[row * block_rows:row * block_rows + diff_row, col * block_cols:col * block_cols + diff_col]
 
 
 def bias(deriv_x, deriv_y, deriv_t, weight_func):
@@ -77,15 +92,14 @@ def show_merged(imgs):
     w,h = imgs[0].shape[1], imgs[0].shape[0]
     img = np.zeros((h, w * imgs_num))
     for index, cur_img in enumerate(imgs):
-        img[0:h, w * index:w * (index + 1)] = cur_img
+            img[0:h, w * index:w * (index + 1)] = cur_img
     plt.imshow(img)
-    # plt.arrow( 0, 0, 100, 100, fc="k", ec="k",head_width=5, head_length=5 )
     plt.show()
 
 
 if __name__ == '__main__':
-    img0_orig = imageio.imread('./../data/flower00.png')
-    img1_orig = imageio.imread('./../data/flower01.png') 
+    img0_orig = imageio.imread('./../data/monkey/monkey5_103.jpg')
+    img1_orig = imageio.imread('./../data/monkey/monkey5_104.jpg') 
     img0, img1 = rgb2gs(img0_orig), rgb2gs(img1_orig)
     
     # img0 = np.array([[0, 0, 0],
@@ -98,30 +112,48 @@ if __name__ == '__main__':
     
     res = []
     
-    block_pairs = zip(img_block(img0, PARTITION), img_block(img1, PARTITION))
+    sigma = 1.5
+    
+    img0_ = scipy.ndimage.gaussian_filter(img0, sigma)
+    img1_ = scipy.ndimage.gaussian_filter(img1, sigma)
+    
+    time_spart = np.matrix([img0_.flatten(), img1_.flatten()])
+    time_spart = scipy.ndimage.gaussian_filter1d(time_spart, sigma / 3, axis=0)
+    
+    img0_ = time_spart[0,:].reshape(img0.shape)
+    img1_ = time_spart[1,:].reshape(img0.shape)
+    show_merged([img0, img1, img0_, img1_])
+    
+    
+    block_pairs = zip(img_block_pixelvice(img0_, BLOCK_SHAPE), img_block_pixelvice(img1_, BLOCK_SHAPE))
     for img, next_img in block_pairs:
         deriv_x, deriv_y = np.zeros(img.shape), np.zeros(img.shape)
         deriv_t = img - next_img
-        scipy.ndimage.sobel(img, axis=0, output=deriv_x, mode="constant")
-        scipy.ndimage.sobel(img, axis=1, output=deriv_y, mode="constant")
+        scipy.ndimage.sobel(img, axis=1, output=deriv_x, mode="constant")
+        scipy.ndimage.sobel(img, axis=0, output=deriv_y, mode="constant")
         b = bias(deriv_x, deriv_y, deriv_t, gaussian2d)
         M = coeff_matrix(deriv_x, deriv_y, gaussian2d)
         trans = np.linalg.pinv(M).dot(b)
         
         res.append(trans)
-        # show_merged([deriv_x, deriv_y, deriv_t])
         
     for item in res:
         print(item)
         
     plt.imshow(img0_orig)
-    block_w, block_h = img0_orig.shape[1] // PARTITION[1], img0_orig.shape[0] // PARTITION[0]
-    half_block_w, half_block_h = block_w // 2, block_h // 2
-    for i in range(PART_Y):
-        for j in range(PART_X):
-            start_row = i * block_h + half_block_h
-            start_col = j * block_w + half_block_w
-            plt.arrow(start_row, start_col, res[i * PART_X + j][1], res[i * PART_X + j][0], fc="k", ec="k",head_width=4, head_length=4 )
+    rows, cols = img0_orig.shape[0], img0_orig.shape[1]
+    block_rows, block_cols = BLOCK_SHAPE
+    col_parts = ceil(cols / block_cols)
+    row_parts = ceil(rows / block_rows)
+    for row in range(row_parts):
+        for col in range(col_parts):
+            diff_row, diff_col = block_rows, block_cols
+            if (row + 1) * block_rows >= rows:
+                diff_row = rows - row * block_rows
+            if (col + 1) * block_cols >= cols:
+                diff_col = cols - col * block_cols
+            diff_row //= 2
+            diff_col //= 2
+            plt.arrow(col * block_cols + diff_col, row * block_rows + diff_row, res[row * col_parts + col][1], res[row * col_parts + col][0], fc="k", ec="k",head_width=2, head_length=2 )
     plt.show()
-    
     
